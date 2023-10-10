2c3212d0c0e6016f9f469a223e69a77ceca10318
msalva <kartalozgur13045@gmail.com>
2023-10-10 15:57:36 +0300

    initial commit

---
+++ demo/src/main/java/com/example/demo/model/Person.java
@@ -0,0 +1,24 @@
+package com.example.demo.model;
+
+import java.util.UUID;
+import com.fasterxml.jackson.annotation.ObjectIdGenerators.UUIDGenerator;
+
+public class Person {
+    private final UUID id;
+    private final String name;
+
+    public Person(UUID id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public UUID getId() {
+        return id;
+    }
+    
+
+    public String getName() {
+        return name;
+    }
+    
+}
