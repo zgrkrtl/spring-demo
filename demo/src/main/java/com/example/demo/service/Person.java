61541510449332f095b2dc4e4aea2abc265d961d
msalva <kartalozgur13045@gmail.com>
2023-10-11 09:32:04 +0300

    initial commit

---
+++ demo/src/main/java/com/example/demo/service/Person.java
@@ -0,0 +1,20 @@
+package com.example.demo.service;
+
+import java.util.UUID;
+
+public class Person {
+    private final UUID id;
+    private final String name;
+    public Person(UUID id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+    public UUID getId() {
+        return id;
+    }
+    public String getName() {
+        return name;
+    }
+    
+    
+}
